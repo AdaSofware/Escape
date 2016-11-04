f4423588816c1a4f6066a53f769897d68cca3e25
AdaSofware <natkin178@live.com>
2016-11-04 15:56:10 +0200

    Add files via upload

---
+++ Assets/Scripts/MainMenuScripts/ExitGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+public class ExitGame : MonoBehaviour {
+
+	public void ExitGameOnClick()
+    {
+//#if UNITY_EDITOR 
+//        UnityEditor.EditorApplication.isPlaying = false;
+//#else
+        Application.Quit();
+//#endif
+        
+    }
+}
